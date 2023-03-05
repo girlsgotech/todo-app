9bcabddaae86ba070faeea1133987ac86a500eb6
girlsgotech <mukisas10@gmail.com>
2023-03-05 04:26:10 +0100

    Added Task list component

--- src/components/list_todo/ListTodo.jsx
+++ src/components/list_todo/ListTodo.jsx
@@ -4,9 +4,20 @@ import "./ListTodo.scss";
 export default function ListTodo() {
   return (
     <div className="listTodo">
-      <div className="listTodo__row"></div>
-      <div className="listTodo__item">Buy New Laptop</div>
-      <div className="listTodo__delete">Del</div>
+      <div className="listTodo__row">
+        <div className="listTodo__item">Buy New Laptop</div>
+        <div className="listTodo__delete">Del</div>
+      </div>
+
+      <div className="listTodo__row">
+        <div className="listTodo__item">Make Codehance Course</div>
+        <div className="listTodo__delete">Del</div>
+      </div>
+
+      <div className="listTodo__row">
+        <div className="listTodo__item">Complete Today's Task</div>
+        <div className="listTodo__delete">Del</div>
+      </div>
     </div>
   );
 }
